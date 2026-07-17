ba795c6326f9372f9e7dc4e79303ee1c3f22669d
Ni-sHa-nt <nk4815027@gmail.com>
2026-07-17 23:37:18 +0530

    Trusted Section Added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Header Section
 window.addEventListener("scroll", function () {
     const header = document.querySelector(".header-sec");
 
@@ -6,4 +7,48 @@ window.addEventListener("scroll", function () {
     } else {
         header.classList.remove("scrolled");
     }
+});
+
+// Trusted Section 
+const marqueeSwiper = new Swiper(".marqueeSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    loopAdditionalSlides: 20,
+    speed: 5000,
+    freeMode: true,
+    allowTouchMove: true,
+    grabCursor: true,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        // Mobile
+        0: {
+            slidesPerView: 3,
+        },
+
+        // Tablet
+        768: {
+            slidesPerView: 5,
+        },
+
+        // Desktop
+        1024: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+const marquee = document.querySelector(".marqueeSwiper");
+
+marquee.addEventListener("mouseenter", () => {
+    marqueeSwiper.autoplay.stop();
+});
+
+marquee.addEventListener("mouseleave", () => {
+    marqueeSwiper.autoplay.start();
 });
